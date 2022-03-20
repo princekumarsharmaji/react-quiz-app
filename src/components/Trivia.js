f9c30424da59b38fd6c8b863f6876366f4ceb34d
Prince Kumar <princekumar@Princes-MacBook-Air.local>
2022-03-20 20:18:29 -0300

    deploy my app

---
+++ src/components/Trivia.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function() {
+  return (
+    <div className='trivia'>
+        <div className='question'>What is best country in the world?</div>
+        <div className='answers'>
+            <div className='answer'>Canada</div>
+            <div className='answer'>India</div>
+            <div className='answer wrong'>America</div>
+            <div className='answer'>Australia</div>
+        </div>
+    </div>
+  )
+}
